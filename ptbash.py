cd46add369c571eb42a07e8ee94571481b21b8a5
Aaron Meurer <asmeurer@gmail.com>
2020-12-31 03:42:03 -0700

    Proof of concept automatically updating prompt with git branch

--- ptbash.py
+++ ptbash.py
@@ -1,7 +1,15 @@
+import os
 import sys
+import subprocess
+import threading
+import time
 
 from pexpect import spawn, EOF
+
 from prompt_toolkit import PromptSession
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 def run():
     # We need to connect bash to a pseudo-terminal to trick it into making the
@@ -17,14 +25,55 @@ def run():
         res = bash.expect_exact([default_bash_prompt, EOF])
         return res == 0
 
+    setup_git_prompt(session)
     while expect_prompt(): # retcode := bash.poll() is not None:
         try:
             print(bash.before.decode('utf-8'), end='')
-            command = session.prompt("$ ")
+            command = session.prompt(ps1())
             bash.send(command.encode('utf-8') + b'\n')
         except EOFError:
             bash.sendeof()
     sys.exit(bash.exitstatus)
+
+def git_prompt():
+    gitprompt = os.environ.get("GIT_PROMPT_FILE")
+    if not gitprompt:
+        return ''
+    p = subprocess.run(
+        ['bash', '-c', f'source {gitprompt};echo $(__git_ps1)'],
+        stdout=subprocess.PIPE, encoding='utf-8')
+    return p.stdout.strip()
+
+def ps1():
+    return git_prompt() + '$ '
+
+def setup_git_prompt(session):
+    # TODO: Handle being in a subdirectory
+    git_dir = '.git'
+
+    class Handler(FileSystemEventHandler):
+        def on_modified(self, event):
+            if event.src_path == os.path.abspath(os.path.join(git_dir, 'HEAD')):
+                callback()
+        on_created = on_modified
+
+    def callback():
+        session.message = ps1()
+        session.app.invalidate()
+
+    def watch():
+        observer = Observer()
+        observer.schedule(Handler(), git_dir)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        finally:
+            observer.stop()
+            observer.join()
+
+    t = threading.Thread(target=watch)
+    t.start()
 
 if __name__ == '__main__':
     run()
